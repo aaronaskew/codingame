2d5e18311a14dcd69006be10c7ee7e4afa136aea
Aaron Askew <aaronaskew@gmail.com>
2023-10-12 12:20:36 -0500

    creating structs

--- puzzles/codingame-sponsored-contest/src/main.rs
+++ puzzles/codingame-sponsored-contest/src/main.rs
@@ -39,91 +39,120 @@ fn main() {
     let second_init_input = parse_input!(input_line, i32);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let num_integer_pairs_per_turn = parse_input!(input_line, i32);
+    let num_characters = parse_input!(input_line, i32);
 
     let init_data = InitData {
         first_init_input,
         second_init_input,
-        num_integer_pairs_per_turn, //the number of characters, one of which is the player
+        num_integer_pairs_per_turn: num_characters, //the number of characters, one of which is the player
     };
 
     let mut characters: Vec<Character> = Vec::new();
-    for _ in 0..num_integer_pairs_per_turn {
+    for _ in 0..num_characters {
         characters.push(Character::new());
     }
-    eprintln!("characters: {characters:#?}"); //dump the characters
+    //eprintln!("characters: {characters:#?}"); //dump the characters
 
     let mut turnData: Vec<TurnIO> = Vec::new();
 
     eprintln!("initData: {:?}", init_data);
 
     let mut turn: usize = 0;
-    let commands = vec![
-        'A', // moves player somehow?
-        'B', // hold still
-        'C', // moves player somehow?
-        'D', // moves player somehow?
-        'E', // moves player somehow?
-    ];
-    let mut last_cmd = String::from("x");
+
+    //Figure out what the commands are
+    let commands = MoveCommands {
+        Up: '?',
+        Down: '?',
+        Left: '?',
+        Right: '?',
+        StayPut: 'B',
+    };
+    //     'A', // moves player somehow?
+    //     'B', // hold still
+    //     'C', // moves player somehow?
+    //     'D', // moves player somehow?
+    //     'E', // moves player somehow?
+
+    let mut last_cmd = 'x';
+
+    // let mut test_cmds: Vec<&str> = Vec::new();
+    // {
+    //     let test_cmds_string = "B";
+    //     for cmd in test_cmds_string.split {
+    //         test_cmds.push(cmd);
+    //     }
+    // }
+
+    //Make a sequence of test commands
+    let test_cmds = vec![commands.StayPut];
+
+    // let test_cmds: Vec<&str> = test_cmds_string
+    //     .char_indices()
+    //     .map(|(i, _)| {
+    //         let next_char_start = test_cmds_string[i..].chars().next().unwrap().len_utf8();
+    //         &test_cmds_string[i..i + next_char_start]
+    //     })
+    //     .collect();
+
+    let commands = test_cmds;
 
     // game loop
     loop {
         eprintln!("begin turn: {}", turn);
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let first_input = input_line.trim_matches('\n').to_string();
+        let char1 = input_line.trim_matches('\n').to_string();
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let second_input = input_line.trim_matches('\n').to_string();
+        let char2 = input_line.trim_matches('\n').to_string();
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let third_input = input_line.trim_matches('\n').to_string();
+        let char3 = input_line.trim_matches('\n').to_string();
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let fourth_input = input_line.trim_matches('\n').to_string();
+        let char4 = input_line.trim_matches('\n').to_string();
 
-        turnData.push(TurnIO {
-            turn: turn,
-            cmd: last_cmd.to_string(),
-            char1: first_input,
-            char2: second_input,
-            char3: third_input,
-            char4: fourth_input,
-            int_pairs: Vec::new(),
-        });
-
-        for i in 0..num_integer_pairs_per_turn as usize {
+        //process the character positions
+        for i in 0..num_characters as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let fifth_input = parse_input!(inputs[0], i32);
-            let sixth_input = parse_input!(inputs[1], i32);
+            let character_pos_x = parse_input!(inputs[0], i32);
+            let character_pos_y = parse_input!(inputs[1], i32);
             //eprintln!("{} {}", fifth_input, sixth_input);
-            turnData[turn as usize]
-                .int_pairs
-                .push((fifth_input, sixth_input));
+
+            characters[i].set_pos(vec![character_pos_x, character_pos_y].into());
+
+            // turnData[turn as usize]
+            //     .int_pairs
+            //     .push((fifth_input, sixth_input));
         }
 
-        //dump turn data
-        for td in &turnData {
-            eprintln!("{:?}", td.to_string());
+        //dump characters
+        //eprintln!("characters: {characters:#?}");
+
+        turnData.push(TurnIO {
+            turn,
+            cmd: last_cmd.to_string(),
+            char1,
+            char2,
+            char3,
+            char4,
+            characters: characters.clone(),
+        });
+
+        //dump turn data (last 10)
+        let i_start: usize = match turnData.len() {
+            x if x < 10 => 0,
+            x => x - 10,
+        };
+        for i in i_start..turnData.len() {
+            eprintln!("{:?}", turnData[i].to_string());
         }
         //eprintln!("turnData: {:#?}", turnData);
 
-        //Make a sequence of test commands
-        let mut test_cmds: Vec<&str> = Vec::new();
-        {
-            let test_cmds_string = "B";
-            for cmd in test_cmds_string.split(" ") {
-                test_cmds.push(cmd);
-            }
-        }
-
-        let commands = test_cmds;
-
         //Send next command
-        last_cmd = commands[turn % commands.len()].to_string();
+        last_cmd = commands[turn % commands.len()];
         //eprintln!("command: {}", last_cmd);
         println!("{}", last_cmd);
 
@@ -131,9 +160,9 @@ fn main() {
         // Increment turn
         turn += 1;
 
-        // if turn > commands.len() {
-        //     break;
-        // }
+        if turn > commands.len() {
+            break;
+        }
     }
 }
 #[derive(Debug)]
@@ -144,7 +173,7 @@ struct TurnIO {
     char2: String,
     char3: String,
     char4: String,
-    int_pairs: Vec<(i32, i32)>,
+    characters: Vec<Character>,
 }
 
 impl TurnIO {
@@ -160,22 +189,37 @@ impl TurnIO {
         s.push_str(&self.char3);
         s.push_str(&self.char4);
         //conv 4 char pattern from binary to decimal
-        s.push_str({
-            let mut binary_string = String::new();
-            for c in [&self.char1, &self.char2, &self.char3, &self.char4].iter() {
-                match c.as_str() {
-                    "_" => binary_string.push_str("0"),
-                    "#" => binary_string.push_str("1"),
-                    _ => binary_string.push_str("x"),
-                }
-            }
-            let binary_val = u32::from_str_radix(&binary_string, 2).unwrap();
-            format![" {}[{:>2}]", binary_string, binary_val].as_str()
-        });
-        s.push_str(" ");
-        for (i1, i2) in &self.int_pairs {
-            s.push_str(format!("({:>2} {:>2})", &i1.to_string(), &i2.to_string()).as_str());
+        // s.push_str({
+        //     let mut binary_string = String::new();
+        //     for c in [&self.char1, &self.char2, &self.char3, &self.char4].iter() {
+        //         match c.as_str() {
+        //             "_" => binary_string.push_str("0"),
+        //             "#" => binary_string.push_str("1"),
+        //             _ => binary_string.push_str("x"),
+        //         }
+        //     }
+        //     let binary_val = u32::from_str_radix(&binary_string, 2).unwrap();
+        //     format![" {}[{:>2}]", binary_string, binary_val].as_str()
+        // });
+
+        // s.push_str(" ");
+        // for (i1, i2) in &self.int_pairs {
+        //     s.push_str(format!("({:>2} {:>2})", &i1.to_string(), &i2.to_string()).as_str());
+        // }
+
+        //print character data
+        for (i, c) in self.characters.iter().enumerate() {
+            let character_string = format![
+                "[c{}{}{}({:>2},{:>2})]",
+                i,
+                if c.has_ever_moved { "$" } else { " " },
+                if c.position_changed() { "!" } else { " " },
+                c.position.as_ref().unwrap()[0],
+                c.position.as_ref().unwrap()[1],
+            ];
+            s.push_str(&character_string);
         }
+
         s
     }
 }
@@ -187,10 +231,12 @@ struct InitData {
     num_integer_pairs_per_turn: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Character {
     last_position: Option<Vec<i32>>,
     position: Option<Vec<i32>>,
+    has_ever_moved: bool,
+    letter: char,
 }
 
 impl Character {
@@ -198,6 +244,8 @@ impl Character {
         Character {
             last_position: None,
             position: None,
+            has_ever_moved: false,
+            letter: '-',
         }
     }
 
@@ -213,9 +261,43 @@ impl Character {
             None => None,
             Some(ref p) => position,
         };
+
+        if self.position_changed() {
+            self.has_ever_moved = true;
+        }
     }
 
     fn position_changed(&self) -> bool {
+        if self.last_position == None {
+            return false;
+        }
+
         self.position != self.last_position
+    }
+}
+
+struct MoveCommands {
+    Up: char,
+    Down: char,
+    Left: char,
+    Right: char,
+    StayPut: char,
+}
+
+struct GameBoard {
+    width: usize,
+    height: usize,
+    char0: Character,
+    char1: Character,
+    char2: Character,
+    char3: Character,
+    char4: Character,
+}
+
+impl GameBoard {
+    fn drawBoard(&self) {
+        for i in 0..self.width {
+            for j in 0..self.height {}
+        }
     }
 }
